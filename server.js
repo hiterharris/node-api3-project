ac3d224def97856a8ae00e8b381f6be5e65fa008
Hiter Harris <hiterharris@comcast.net>
2020-02-12 15:41:11 -0500

    added logger middleware

--- server.js
+++ server.js
@@ -5,17 +5,19 @@ const userRouter = require('./users/userRouter');
 const server = express();
 
 server.use(express.json());
+server.use(logger);
 
-
-server.get('/', (req, res) => {
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 server.use('/users', userRouter);
 
-
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} request to ${req.originalUrl}`); // Add timestamp
+  next();
+}
 
 module.exports = server;
