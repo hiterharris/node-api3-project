0333a6f3534c536baa7df423ca3ad3a97e2486e7
Hiter Harris <hiterharris@comcast.net>
2020-02-13 13:33:12 -0500

    made port dynamic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const server = require('./server.js');
 
-server.listen(3001, () => {
-    console.log('Server listening on port 3001');
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
 });
